0d04655ce65f5ccb356a1a69d7c4ec805ab85895
Polsander <oerdmann@uwaterloo.ca>
2024-01-26 17:34:48 -0500

    T based dewpoint function

--- HW1/ethylene_to_ethanoly.py
+++ HW1/ethylene_to_ethanoly.py
@@ -3,7 +3,7 @@ Oliver Erdmann
 20843970
 '''
 import numpy as np;
-from automated_case_solvers import case1_solver, calc_bubble_point;
+from automated_case_solvers import case1_solver, calc_bubble_point, calc_dew_point;
 
 class Epsilon: # Using this class to create my Epsilon object with a value and key position (index).
     def __init__(self, value: float, position: int) -> None:
@@ -39,4 +39,6 @@ u2 = np.array([680.72, 90.79, 1198.77, 2.421, 200, 266.71, 1.8802 ]);
 # case1_solver(epsilon, antoine_coeffs, 313.15, Pressure, u2, specification='P', tolerance=0.06);
 
 #Execute bubble point solver
-calc_bubble_point(Pressure, 310, u2, antoine_coeffs, maxiter=1000, tol=2)
+calc_bubble_point(Pressure, 310, u2, antoine_coeffs, specification='P', maxiter=1000, tol=2)
+
+calc_dew_point(Pressure, 390, u2, antoine_coeffs, specification='P', tol=0.1 )
