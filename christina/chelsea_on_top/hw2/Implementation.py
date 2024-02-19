ca763f008d46674f5b7f4e4a5bfb4917d25a65be
NeymarTheHazard <157079817+NeymarTheHazard@users.noreply.github.com>
2024-02-19 00:01:01 -0500

    updated mass balances and temperatures

--- christina/chelsea_on_top/hw2/Implementation.py
+++ christina/chelsea_on_top/hw2/Implementation.py
@@ -82,9 +82,9 @@ mass_EL = np.array([mu01el,mu1el,mu2el,mu31el,mu32el,mu41el,mu42el,mu51el,mu52el
 #print(mass_EL)
 
 #Water
-mu02w = 0.6*mu01el
-mu1w = mu02w/(1-(eps_abs_V[0]*eps_flash[0]*(1-eta)))
-mu2w = (1-eta)*mu1w
+mu2w = 0.6*mu2el
+mu1w = mu2w/(1-eta)
+#mu1w = mu02w/(1-(eps_abs_V[0]*eps_flash[0]*(1-eta)))
 mu31w = eps_flash[0]*mu2w
 mu32w = (1-eps_flash[0])*mu2w
 mu41w = (eps_abs_V[0])*mu31w
@@ -94,17 +94,17 @@ mu52w = 0
 mu6w = mu32w + mu42w
 mu71w = 0.1*mu6w
 mu72w = 0.9*mu6w
-mu81w = mu71w
+mu81w = 0
 mu82w = mu72w
 
-mass_W = np.array([mu02w,mu1w,mu2w,mu31w,mu32w,mu41w,mu42w,mu51w,mu52w,mu6w,mu71w, mu72w, mu81w, mu82w])
+mass_W = np.array([mu1w,mu2w,mu31w,mu32w,mu41w,mu42w,mu51w,mu52w,mu6w,mu71w, mu72w, mu81w, mu82w])
 #print('Water Linear Mass Balance')
-#print('mu02,mu1,mu2,mu31,mu32,mu41,mu42,mu51,mu52, mu6, mu71, mu72, mu81, mu82')
+#print('mu1,mu2,mu31,mu32,mu41,mu42,mu51,mu52, mu6, mu71, mu72, mu81, mu82')
 #print(mass_W)
 
 #Ethanol
 mu1ea = 1/(1-(eps_abs_V[1]*eps_flash[1]*(1+eta))-((1-eps_flash[1])*(1+eta)+((1-eps_abs_V[1])*eps_flash[1]*(1+eta)))*0.005*0.995)
-mu2ea = (1+eta)*mu1ea
+mu2ea = mu1ea + (eta)*mu1el
 mu31ea = eps_flash[1]*mu2ea
 mu32ea = (1-eps_flash[1])*mu2ea
 mu41ea = (eps_abs_V[1])*mu31ea
@@ -125,7 +125,7 @@ mass_EA = np.array([mu1ea,mu2ea,mu31ea,mu32ea,mu41ea,mu42ea,mu51ea,mu52ea,mu6ea,
 #print(mass_EA)
 
 #Diethyl Ether
-mu1d = 1/(1-(eps_abs_V[3]*eps_flash[3]*K*(mu2ea**2)/mu2w)-((1-eps_flash[3])*K*(mu2ea**2)/mu2w +((1-eps_abs_V[3])*eps_flash[3]*K*(mu2ea**2)/mu2w)))
+#mu1d = 1/(1-(eps_abs_V[3]*eps_flash[3]*K*(mu2ea**2)/mu2w)-((1-eps_flash[3])*K*(mu2ea**2)/mu2w +((1-eps_abs_V[3])*eps_flash[3]*K*(mu2ea**2)/mu2w)))
 mu2d = K*(mu2ea**2)/mu2w
 mu31d = eps_flash[3]*mu2d
 mu32d = (1-eps_flash[3])*mu2d
@@ -136,10 +136,11 @@ mu52d = 0
 mu6d = mu32d + mu42d
 mu71d = mu6d
 mu72d = 0
-mu81d = 0
-mu82d = mu71d
+mu81d = 0.995*mu71d
+mu82d = 0.005*mu71d
 mu91d = mu82d
 mu92d = 0
+mu1d = (eps_abs_V[3])*mu41d + 0.995*mu81d
 
 mass_DEE = np.array([mu1d,mu2d,mu31d,mu32d,mu41d,mu42d,mu51d,mu52d,mu6d,mu71d, mu72d, mu81d, mu82d, mu91d, mu92d])
 #print('Diethyl Ether Linear Mass Balance')
@@ -148,46 +149,51 @@ mass_DEE = np.array([mu1d,mu2d,mu31d,mu32d,mu41d,mu42d,mu51d,mu52d,mu6d,mu71d, m
 
 #Temperature Levels
 #Stream 1
-# mu1 = np.array([mu1w,mu1ea,mu1el,mu1d])
-# T1 = bubble_point(750,300,mu1,A,B,C,Find_T=True)
+mu1 = np.array([mu1w,mu1ea,mu1el,mu1d])
+T1 = bubble_point(750,300,mu1,A,B,C,Find_T=True)
 
 #Stream 31 and 32
 mu2 = np.array([mu2w,mu2ea,mu2el,mu2d])
 #mu31_OLD= np.array([mu31w,mu31ea,mu31el,mu31d]) 
-#mu32_OLD= np.array([mu32w,mu32ea,mu32el,mu32d])
-# breakpoint()
+#mu32_OLD= np.array([mu32w,mu32ea,mu32el,mu32d]) 
 flash1 = case1_flash(0.5,68.5*750,393, mu2, A,B,C, 2)
 mu31 = flash1[2]*flash1[3]
 mu32 = flash1[1]*flash1[4]
-# print(mu31,mu32)
+
+#print(mu31,mu32)
 
 #Stream 03, 41, 42
-absorber1 = absorber(mu31,68.5*750, 300,A,B,C,2)
+absorber1 = absorber(mu31,68.5*750, 300,A,B,C,3)
 mu03 = absorber1[1]
 mu41 = absorber1[2]
 mu42 = absorber1[3]
+
 #print(mu03, mu41, mu42)
 
 #Stream 6
 mu6 = mu32+mu42
 T6 = bubble_point(68.5*750, 350, mu6, A, B, C, Find_T=True)
-
+#print(mu6,T6)
 #Stream 71 and 72
 distillation1 = distillation(mu6,T6,20*750,(1,0.995),(0,0.005),A,B,C)
 mu71 = distillation1[1]
 mu72 = distillation1[0]
 Tdist1 = distillation1[4]
-#print(mu71,mu72)
+Tbot1 = distillation1[5]
+#print(mu71,mu72, Tdist1, Tbot1)
 
 #Stream 81 and 82
 distillation2 = distillation(mu71,Tdist1,10*750,(3,0.995),(1,0.005),A,B,C)
 mu81 = distillation2[1]
 mu82 = distillation2[0]
+Tdist2 = distillation2[4]
 Tbot2 = distillation2[5]
-#print(mu81,mu82)
+#print(mu81,mu82,Tdist2, Tbot2)
 
 #Stream 91 and 92
 distillation3 = distillation(mu82,Tbot2,1*750,(1,0.995),(2,0.005),A,B,C)
 mu91 = distillation3[1]
 mu92 = distillation3[0]
-#print(mu91,mu92)
+Tdist3 = distillation3[4]
+Tbot3 = distillation3[5]
+#print(mu91,mu92,Tdist3, Tbot3)
